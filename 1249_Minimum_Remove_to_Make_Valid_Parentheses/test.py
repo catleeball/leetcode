e959d2ac1e6a22a958c572fb068939d503dab9f5
🎷🐛 Cat Lee Ball <cat@catball.dev>
2021-08-11 16:24:54 -0700

    Add test cases from question's examples

--- 1249_Minimum_Remove_to_Make_Valid_Parentheses/test.py
+++ 1249_Minimum_Remove_to_Make_Valid_Parentheses/test.py
@@ -7,5 +7,23 @@ class TestSolution(unittest.TestCase):
     def setUp(self):
         self.subarraySum = solution.Solution().subarraySum
 
+    def test_example_1(self):
+        input_string = "lee(t(c)o)de)"
+        # Any of following are acceptable outputs.
+        expected = ["lee(t(c)o)de", "lee(t(co)de)", "lee(t(c)ode)"]
+
+    def test_example_2(self):
+        input_string = "a)b(c)d"
+        expected = "a)b(c)d"
+
+    def test_example_3(self):
+        input_string = "))(("
+        expected = ""
+
+    def test_example_4(self):
+        input_string = "(a(b(c)d)"
+        expected = "a(b(c)d)"
+
+
 if __name__ == '__main__':
     unittest.main()
