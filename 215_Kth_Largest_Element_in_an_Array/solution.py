715faac0e75e455e6c3c545ce162093edf0acb23
🎷🐛 Cat Lee Ball <cat@catball.dev>
2021-08-12 19:03:27 -0700

    215. Add sort and heap solutions.
    Still should implement a quickselect solution.

--- 215_Kth_Largest_Element_in_an_Array/solution.py
+++ 215_Kth_Largest_Element_in_an_Array/solution.py
@@ -11,8 +11,22 @@ Constraints:
 """
 
 from typing import List
+import heapq
 
 
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        pass
+    # Sort solution. Compute: O(n log n), Memory: O(1)
+    # Stats from Leetcode:
+    #     Runtime:       140 ms, faster than 17.27% of Python3 online submissions for Kth Largest Element in an Array.
+    #     Memory Usage: 15.1 MB, less than   45.82% of Python3 online submissions for Kth Largest Element in an Array.
+    def findKthLargest_sort(self, nums: List[int], k: int) -> int:
+        return sorted(nums)[-k]
+
+    # Heapq solution. Compute: O(n log k), Memory: O(k)
+    # Stats from Leetcode:
+    #     Runtime:        64 ms, faster than 70.63% of Python3 online submissions for Kth Largest Element in an Array.
+    #     Memory Usage: 15.1 MB, less than 75.49%   of Python3 online submissions for Kth Largest Element in an Array.
+    def findKthLargest_heap(self, nums: List[int], k: int) -> int:
+        return heapq.nlargest(k, nums)[-1]
+
+    # TODO: Quickselect solution (https://en.wikipedia.org/wiki/Quickselect).
--- 215_Kth_Largest_Element_in_an_Array/test.py
+++ 215_Kth_Largest_Element_in_an_Array/test.py
@@ -6,21 +6,28 @@ from typing import List
 class TestSolution(unittest.TestCase):
 
     def setUp(self):
-        self.minRemoveToMakeValid = Solution().findKthLargest
+        self.findKthLargest_sort = Solution().findKthLargest_sort
+        self.findKthLargest_heap = Solution().findKthLargest_heap
 
     def test_example_1(self):
         nums: List[int] = [3, 2, 1, 5, 6, 4]
         k: int          = 2
         expected: int   = 5
-        actual = self.findKthLargest(nums, k)
-        self.assertEqual(expected, actual)
+
+        sort_solution = self.findKthLargest_sort(nums, k)
+        heap_solution = self.findKthLargest_heap(nums, k)
+        self.assertEqual(expected, sort_solution)
+        self.assertEqual(expected, heap_solution)
 
     def test_example_2(self):
         nums: List[int] = [3, 2, 3, 1, 2, 4, 5, 5, 6]
         k: int          = 4
         expected: int   = 4
-        actual = self.findKthLargest(nums, k)
-        self.assertEqual(expected, actual)
+
+        sort_solution = self.findKthLargest_sort(nums, k)
+        heap_solution = self.findKthLargest_heap(nums, k)
+        self.assertEqual(expected, sort_solution)
+        self.assertEqual(expected, heap_solution)
 
 
 if __name__ == '__main__':
