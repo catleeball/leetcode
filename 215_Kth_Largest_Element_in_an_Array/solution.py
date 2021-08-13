f095ae6dc0f8ec585414a80d3a17c103b02c7b31
🎷🐛 Cat Lee Ball <cat@catball.dev>
2021-08-12 17:25:41 -0700

    215. Add docstring with problem statement

--- 215_Kth_Largest_Element_in_an_Array/solution.py
+++ 215_Kth_Largest_Element_in_an_Array/solution.py
@@ -1,3 +1,15 @@
+"""
+Leetcode problem #215: Kth Largest Element in an Array
+URL: https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Constraints:
+    1 <= k <= nums.length <= 104
+    -10^4 <= nums[i] <= 10^4
+"""
+
 from typing import List
 
 
