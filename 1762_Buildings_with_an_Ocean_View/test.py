21ca5b50b7489c9211480bedb47545c28e94aa23
Cat Lee Ball <cat@catball.dev>
2021-10-09 14:37:53 -0700

    Add example test cases for 1762

--- 1762_Buildings_with_an_Ocean_View/test.py
+++ 1762_Buildings_with_an_Ocean_View/test.py
@@ -1,10 +1,22 @@
 import unittest
 from solution import Solution
+from absl.testing import parameterized
 
-class TestSolution(unittest.TestCase):
 
-    def test_example_1(self):
-        pass
+class TestSolution(parameterized.TestCase):
+    @parameterized.parameters(
+        {'heights': [4, 2, 3, 1], 'expected': [0, 2, 3]   },
+        {'heights': [4, 3, 2, 1], 'expected': [0, 1, 2, 3]},
+        {'heights': [1, 3, 2, 4], 'expected': [3]         },
+        {'heights': [2, 2, 2, 2], 'expected': [3]         },
+    )
+    def setUp(self) -> None:
+        self.c = Solution()
+        self.findBuildings = Solution.findBuildings
+
+    def test_example_1(self, heights, expected):
+        acutal = self.findBuildings(heights)
+        self.assertEqual(expected, acutal)
 
 
 if __name__ == '__main__':
