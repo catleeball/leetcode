cb08f29a983ae50d4466b79bc19fb5a1a9f28662
Cat Lee Ball <cat@catball.dev>
2021-10-09 14:16:53 -0700

    Add test skeleton for 1762

---
+++ 1762_Buildings_with_an_Ocean_View/test.py
@@ -0,0 +1,11 @@
+import unittest
+from solution import Solution
+
+class TestSolution(unittest.TestCase):
+
+    def test_example_1(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
